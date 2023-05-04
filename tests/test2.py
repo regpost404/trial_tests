934145a295345643870fe7e379daecb5664d2b16
regpost404 <regpost404@gmail.com>
2023-05-04 14:01:16 +0300

    auto close

---
+++ tests/test2.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
+o = Options()
+o.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=o)
+driver.get('https://www.google.com/?hl=en')
+input_google = driver.find_element("name","q")
+input_google.send_keys("hello qa")
+input_google.send_keys(Keys.RETURN)
+assert "hello qa - Google Search" in driver.title
+driver.close()
+
