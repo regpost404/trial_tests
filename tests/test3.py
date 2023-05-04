3457f71cf60453e61de1d37e71e7cee3d51ffb91
regpost404 <regpost404@gmail.com>
2023-05-04 14:36:14 +0300

    auto close on

---
+++ tests/test3.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+o = Options()
+o.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=o)
+# driver.set_window_size(1920,1080)
+driver.maximize_window()
+driver.get('https://outlook.live.com/owa/')
+bt_log_in = driver.find_element(By.XPATH, "/html/body/header/div/aside/div/nav/ul/li[2]/a").click()
+input_login = driver.find_element(By.NAME, "loginfmt")
+input_login.send_keys("dominicpf9@outlook.com")
+input_next = driver.find_element(By.ID, "idSIButton9").click()
+input_pass = driver.find_element(By.NAME, "passwd")
+input_pass.send_keys("rumvt16Mt")
+time.sleep(2)
+submit = driver.find_element(By.ID, "idSIButton9").click()
+time.sleep(2)
+no_back = driver.find_element(By.ID, "idBtn_Back").click()
+
+# bt_log_in = driver.find_element(By.CLASS_NAME, "button__empty").click()
+# bt_log_in = driver.find_element(By.XPATH, "/html/body/header/div[2]/a[2]").click()
+
